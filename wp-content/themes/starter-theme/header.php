d686ebc7ca9f1f5d1af34fe0dfcb9de1e3fb6b1f
Matthew-Hurrell <matthewhobbshurrell@icloud.com>
2023-06-02 22:51:32 +0100

    Add basic styling for header menu

--- wp-content/themes/starter-theme/header.php
+++ wp-content/themes/starter-theme/header.php
@@ -5,4 +5,10 @@
 </head>
 <body>
 
-HEADER
+<div class="bg-grey-50 w-full h-20 px-5">
+    <div class="flex justify-between items-center container mx-auto">
+        <div class="h-16">
+            <img class="object-center object-contain h-full" src="<?php echo get_theme_file_uri('/assets/one-stop-inventories.logo.png') ?>" alt="">
+        </div>
+    </div>
+</div>
