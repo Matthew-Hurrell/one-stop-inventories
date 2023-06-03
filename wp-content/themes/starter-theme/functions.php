043218809eb6ecc58e9da596d3a3402f7e8866e8
Matthew-Hurrell <matthewhobbshurrell@icloud.com>
2023-06-04 00:47:42 +0100

    Add hero block plugin

--- wp-content/themes/starter-theme/functions.php
+++ wp-content/themes/starter-theme/functions.php
@@ -8,3 +8,21 @@
     }
 
     add_action('wp_enqueue_scripts', 'wp_enqueue_custom_files');
+
+
+// Add custom block category
+function register_custom_category( $categories ) {
+
+    $categories[] = array(
+        'slug'  => 'custom-category',
+        'title' => 'Custom'
+    );
+
+    return $categories;
+}
+
+if ( version_compare( get_bloginfo( 'version' ), '5.8', '>=' ) ) {
+    add_filter( 'block_categories_all', 'register_custom_category' );
+} else {
+    add_filter( 'block_categories', 'register_custom_category' );
+}
